377b252469a3ad473299bdf32b79bc4c85e5b48f
EdouardQN <119489246+EdouardQN@users.noreply.github.com>
2024-11-25 11:09:48 +0100

    Fix
    fixed an  issue causing the json file to not be read properly by git-hub pages while fetching (interpreted as html).

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,30 @@ import '../styles/Home.css'
 import Banner from '../layout/Banner'
 import Card from '../layout/Card'
 
+
 export default function Home() {
 
   const [logements, setLogements] = useState(null)
 
   useEffect(() => {
-    fetch('logements.json')
-    .then(response => {
+    fetch('/kasa/logements.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
       return response.json();
     })
     .then(data => {
       setLogements(data);
     })
+    .catch(error => {
+      console.error('Error fetching logements.json:', error);
+    });
   }, [])
 
   return (
     <>
+    
       <Banner title = "Chez vous, partout et ailleurs" class='banner' />
       <div className='card-container'>
         {logements && logements.map(
