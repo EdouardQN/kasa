c0f4e40bfcd51226f9a1967a73895076a9a48a1b
EdouardQN <119489246+EdouardQN@users.noreply.github.com>
2024-11-25 11:52:17 +0100

    Fix
    Retrying with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import {
-  createHashRouter, 
-  createRoutesFromElements,
-  Route, 
-  RouterProvider
-} from 'react-router-dom'
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from 'react-router-dom';
 
-// pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Logement from './pages/Logement'
 import Erreur404 from './pages/Erreur404'
-
-//layout
 import Root from './layout/Root'
-
-const router = createHashRouter(  // Utiliser HashRouter ici
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="/logement/:id" element={<Logement />} />
-      <Route path="*" element={<Erreur404 />} />
-    </Route>
-  )
-)
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="logement/:id" element={<Logement />} />
+          <Route path="*" element={<Erreur404 />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
